cb82d00d1f8f02a8f08f21a4225637e2d12ac8b8
Argupta99 <178310155+Argupta99@users.noreply.github.com>
2024-10-26 10:58:08 +0530

    Another nested element eg

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-//creating nested element
+//creating nested element where there is 2 siblings
 const mainHeading = React.createElement("div", {id: "parent"},
     React.createElement("div", {id: "child"},
-        React.createElement("h1", {}, "Hey I am Arpita"))
-);
+       [ React.createElement("h1", {}, "Hey I am Arpita"),React.createElement("h2", {},"Happy Holi to everyone") ]))
+       ;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(mainHeading);
