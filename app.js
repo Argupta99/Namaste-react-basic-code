55ee247ca59681f212bceb15647ea616bc1d2002
Argupta99 <178310155+Argupta99@users.noreply.github.com>
2024-10-26 10:48:18 +0530

    created nested element

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const heading = React.createElement("h1", 
-    {
-    id: "heading"}, 
-"Hello from React");
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(heading);
+//creating nested element
+const mainHeading = React.createElement("div", {id: "parent"},
+    React.createElement("div", {id: "child"},
+        React.createElement("h1", {}, "Hey I am Arpita"))
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(mainHeading);
